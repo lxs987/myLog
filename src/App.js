e3284b8a56c6a321ae5d34c0d0cab6ef99533257
Jinkyung <lxsz987@gmail.com>
2022-05-31 17:37:15 +0900

    edit main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+// html이 아닌 JSX임
+// 자바스크립트 안에서 HTML을 작성할 수 있도록 함
 function App() {
+  let post = "블로그 글 제목";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          짜라자라잔짠짠
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "black-nav">
+        <h4>myLog</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }

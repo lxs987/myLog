3ec8163b0e977bbc118a32b2eddf5cce7ffed35c
Jinkyung <lxsz987@gmail.com>
2022-05-31 17:21:14 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          짜라자라잔짠짠
         </p>
         <a
           className="App-link"
